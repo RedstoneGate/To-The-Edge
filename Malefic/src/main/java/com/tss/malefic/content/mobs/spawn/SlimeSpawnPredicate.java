872a65127daaeeb2340bf8ce2c330de1bd2c297f
haneki <hanekihyouka@gmail.com>
2024-06-15 21:18:36 +0800

    add SlimeSpawnPredicate, you may change your y offset here.

---
+++ Malefic/src/main/java/com/tss/malefic/content/mobs/spawn/SlimeSpawnPredicate.java
@@ -0,0 +1,33 @@
+package com.tss.malefic.content.mobs.spawn;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.levelgen.WorldgenRandom;
+
+import static net.minecraft.world.entity.Mob.checkMobSpawnRules;
+
+public class SlimeSpawnPredicate implements SpawnPlacements.SpawnPredicate{
+
+    @Override
+    public boolean test(EntityType entityType, ServerLevelAccessor level, MobSpawnType mobSpawnType, BlockPos blockPos, RandomSource randomSource) {
+        if (level.getDifficulty() != Difficulty.PEACEFUL){
+            if (!mobSpawnType.equals(MobSpawnType.SPAWNER)){
+                ChunkPos chunkpos = new ChunkPos(blockPos);
+                //flag check slime chunk
+                boolean flag = WorldgenRandom.seedSlimeChunk(chunkpos.x, chunkpos.z, ((WorldGenLevel) level).getSeed(), 987234911L).nextInt(10) == 0;
+                if (randomSource.nextInt(10) == 0 && flag && blockPos.getY() < 40) {
+                    return checkMobSpawnRules(entityType, level, mobSpawnType, blockPos, randomSource);
+                }
+            }
+            return checkMobSpawnRules(entityType,level,mobSpawnType,blockPos,randomSource);
+        }
+        return false;
+    }
+}
